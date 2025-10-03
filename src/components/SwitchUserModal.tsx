98d144bc80f3a6e5075d25057fe094662d3d252e
Lokesh Burade <lokeshburade007@gmail.com>
2025-10-03 15:43:42 +0530

    Here I have fixed Already login error from Switch Modal and Programatically Login Method So if user is loading Alredy then it logout that user and login again.

--- src/components/SwitchUserModal.tsx
+++ src/components/SwitchUserModal.tsx
@@ -23,6 +23,14 @@ export const SwitchUserModal: React.FC<
 
   const handleSwitchUser = async (user: LoggedInUser) => {
     setCurrentUser(user);
+      const currentLoggedInUser = aioha.getCurrentUser();
+      const otherLogins = aioha.getOtherLogins();
+      if (currentLoggedInUser === user.username.trim()) {
+        aioha.logout();
+        await new Promise(resolve => setTimeout(resolve, 500));
+      } else if (otherLogins && otherLogins[user.username.trim()]) {
+        AuthService.removeUser(aioha, user.username.trim());
+      }
     if (user.privatePostingKey) {
       await AuthService.switchUserWithPrivatePostingKey(aioha, user.username, user.privatePostingKey, onSignMessage(user.username.trim().toLocaleLowerCase()));
     }
